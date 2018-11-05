a24acc7399669dfc2f40bcf8f8117e03f07e9f63
farjasju <julesfarjas@gmail.com>
2018-11-05 11:33:32 +0100

    Adding a force_protocol function

---
+++ test/force_protocol_test.py
@@ -0,0 +1,50 @@
+# =============================================================================
+# Ural URL Ensure Protocol Unit Tests
+# =============================================================================
+from ural import force_protocol
+
+TESTS = [
+    ('http://lemonde.fr///a/./b/..', 'http://lemonde.fr///a/./b/..'),
+    ('lemonde.fr/index.html', 'http://lemonde.fr/index.html'),
+    ('lemonde.fr/index.php', 'http://lemonde.fr/index.php'),
+    ('lemonde.fr/index/', 'http://lemonde.fr/index/'),
+    ('lemonde.fr/index.php?utm_content=whatever&test=toto',
+     'http://lemonde.fr/index.php?utm_content=whatever&test=toto'),
+    ('lemonde.fr/index.php?utm_content=whatever',
+     'http://lemonde.fr/index.php?utm_content=whatever'),
+    ('https://lemonde.fr?', 'http://lemonde.fr?'),
+    ('https://lemonde.fr#anchor', 'http://lemonde.fr#anchor'),
+    ('https://lemonde.fr/#anchor', 'http://lemonde.fr/#anchor'),
+    ('https://lemonde.fr/#/path/is/here', 'http://lemonde.fr/#/path/is/here'),
+    ('https://lemonde.fr#!/path/is/here', 'http://lemonde.fr#!/path/is/here'),
+    ('//www.lemonde.fr', 'http://www.lemonde.fr'),
+    ('http://www2.lemonde.fr/index.html', 'http://www2.lemonde.fr/index.html'),
+    ('http://m.lemonde.fr/index.html', 'http://m.lemonde.fr/index.html'),
+    ('http://mobile.lemonde.fr/index.html', 'http://mobile.lemonde.fr/index.html'),
+    ('https://en.m.wikipedia.org/wiki/Ulam_spiral',
+     'http://en.m.wikipedia.org/wiki/Ulam_spiral'),
+    ('http://lemonde.fr?XTOR=whatever', 'http://lemonde.fr?XTOR=whatever'),
+    ('http://lemonde.fr?xtref=1&xtcr=2&xts=3&xtnp=3&xtloc=4',
+     'http://lemonde.fr?xtref=1&xtcr=2&xts=3&xtnp=3&xtloc=4'),
+    ('lemonde.fr?utm_hp_ref=test', 'http://lemonde.fr?utm_hp_ref=test'),
+    ('http://lemonde.fr?ref=fb', 'http://lemonde.fr?ref=fb'),
+    ('http://lemonde.fr?ref=tw', 'http://lemonde.fr?ref=tw'),
+    ('http://lemonde.fr?ref=tw_i', 'http://lemonde.fr?ref=tw_i'),
+    ('http://lemonde.fr?platform=hootsuite',
+     'http://lemonde.fr?platform=hootsuite'),
+    ('lemonde.fr?__twitter_impression=true',
+     'http://lemonde.fr?__twitter_impression=true'),
+    ('https://www4.lemonde.fr?een=34&seen=3458474',
+     'http://www4.lemonde.fr?een=34&seen=3458474'),
+    ('https://www4.lemonde.fr?amp', 'http://www4.lemonde.fr?amp'),
+    ('https://www4.lemonde.fr?amp_analytics=324',
+     'http://www4.lemonde.fr?amp_analytics=324')
+]
+
+
+class TestEnsureProtocol(object):
+    def test_basics(self):
+        for url, output in TESTS:
+            assert force_protocol(url) == output
+        assert force_protocol('http://lemonde.fr?utm_hp_ref=test',
+                              'ftp') == 'ftp://lemonde.fr?utm_hp_ref=test'
---
+++ ural/force_protocol.py
@@ -0,0 +1,30 @@
+# =============================================================================
+# Ural URL Ensure Protocol Function
+# =============================================================================
+#
+# A function checking if the url features a protocol, and adding one if there is none.
+#
+import re
+
+PROTOCOL_RE = re.compile('^[^:]*:?//')
+
+
+def force_protocol(url, protocol='http'):
+    """
+    Function force-replacing the url protocol by the given one (and adding it if there is none).
+
+    Args:
+        url (str): Target URL as a string.
+        protocol (str): protocol wanted. Is 'http' by default.
+
+    Returns:
+        string: The protocol-equipped url.
+
+    """
+    if not PROTOCOL_RE.match(url):
+        url = protocol + '://' + url
+    elif url[:2] == '//':
+        url = protocol + ':' + url
+    else:
+        url = re.sub(PROTOCOL_RE, protocol + '://', url)
+    return url
