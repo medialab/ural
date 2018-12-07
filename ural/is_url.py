dc98cda38d3e712479f7776cc908cf6513e67139
farjasju <julesfarjas@gmail.com>
2018-12-07 12:06:38 +0100

    Storing the url regex in patterns

--- ural/is_url.py
+++ ural/is_url.py
@@ -6,12 +6,7 @@
 #
 from __future__ import unicode_literals
 import re
-from ural.patterns import PROTOCOL_RE
-
-URL_REGEX = r'^([a-zA-Z0-9]*:?//)?(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$'
-
-protocol_pattern = re.compile(PROTOCOL_RE)
-url_pattern = re.compile(URL_REGEX)
+from ural.patterns import PROTOCOL_RE, URL_RE
 
 
 def is_url(string, require_protocol=True):
@@ -26,6 +21,6 @@ def is_url(string, require_protocol=True):
         bool: True if the argument is a url, False if not.
     """
     if require_protocol:
-        return bool(protocol_pattern.match(string) and url_pattern.match(string))
+        return bool(PROTOCOL_RE.match(string) and URL_RE.match(string))
     else:
-        return bool(url_pattern.match(string))
+        return bool(URL_RE.match(string))
