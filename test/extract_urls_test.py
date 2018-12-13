bdd40b6ee4d158278d2fcdec25634d844470245f
farjasju <julesfarjas@gmail.com>
2018-12-13 11:25:16 +0100

    Formatting test code

--- test/extract_urls_test.py
+++ test/extract_urls_test.py
@@ -10,15 +10,13 @@ Today @jovialjoy's @AJLUnited and @GeorgetownCPT are launching the Safe Face Ple
 Now accepting submissions for the 2018 Excellence in Local News Awards http://twib.in/l/xLzxjnpMXx7X  via @medium http://foo.com/blah_(wikipedia)#cite-1
 Directed to help #Alzheimers patients + others w/ impaired memory by providing intuitive ways to benefit from large amounts of personal data Check out this post by @physicspod in @singularityhub http://on.su.org/2rsPeXh"""
 
+REF_SET = set(["http://mitsha.re/Qg1g30mVD78",
+               "http://www.safefacepledge.org",
+               "http://twib.in/l/xLzxjnpMXx7X",
+               "http://on.su.org/2rsPeXh",
+               "http://foo.com/blah_(wikipedia)#cite-1"])
+
 
 class TestStripProtocol(object):
     def test_basics(self):
-        ref_set = set(["http://mitsha.re/Qg1g30mVD78",
-                       "http://www.safefacepledge.org",
-                       "http://twib.in/l/xLzxjnpMXx7X",
-                       "http://on.su.org/2rsPeXh",
-                       "http://foo.com/blah_(wikipedia)#cite-1"])
-        result_set = set()
-        for url in extract_urls(TEXT):
-            result_set.add(url)
-        assert result_set == ref_set
+        assert set(extract_urls(TEXT)) == REF_SET
