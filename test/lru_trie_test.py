8388abd5c552e3998a4c385bca92fbc55b3c8631
farjasju <julesfarjas@gmail.com>
2019-01-14 11:45:50 +0100

    Adding tests

--- test/lru_trie_test.py
+++ test/lru_trie_test.py
@@ -6,8 +6,17 @@
 from ural import LRUTrie
 
 
-class TestIsUrl(object):
+class TestLRUTrie(object):
     def test_basics(self):
         trie = LRUTrie()
         trie.set('http://www.lemonde.fr', {'media': 'lemonde'})
+        trie.set('http://www.lemonde.fr/politique/article',
+                 {'media': 'lemonde', 'type': 'article'})
         assert trie.match('http://www.lemonde.fr') == {'media': 'lemonde'}
+        assert trie.match(
+            'http://www.lemonde.fr/politique/') == {'media': 'lemonde'}
+        assert trie.match(
+            'http://www.lemonde.fr/politique/article') == {'media': 'lemonde', 'type': 'article'}
+        assert trie.match(
+            'http://www.lemonde.fr/politique/article/randompath') == {'media': 'lemonde', 'type': 'article'}
+        assert trie.match('http://www.legorafi.fr') == None
