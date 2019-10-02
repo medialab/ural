28e5402dfb395f98febe130f081b0652f03c183e
Yomguithereal <guillaumeplique@gmail.com>
2019-10-02 16:38:20 +0200

    Fixing url regex

--- test/is_url_test.py
+++ test/is_url_test.py
@@ -7,6 +7,7 @@ from ural import is_url
 
 DEFAULT_TESTS = [
     ('https://www.example.com/foo/?bar=baz&inga=42&quux', True),
+    ('http://www.outremers360.comtalent-de-la-semaine-la-designer-comorienne-aisha-wadaane-je-suis-fiere-de-mes-origines/', False),
     ('lemonde.fr/index.html', False),
     ('http://foo.com/blah_(wikipedia)#cite-1', True),
     ('http://223.255.255.254', True),
@@ -18,13 +19,14 @@ DEFAULT_TESTS = [
     (u'http://مثال.إختبار', True),
     ('http://a.b-.co', False),
     ('http://.www.foo.bar/', False),
-    ('http://www.foo.bar./', False),
+    ('http://www.foo.bar./', True),
     ('', False),
     ('    ', False)
 ]
 
 NO_PROTOCOL_TESTS = [
     ('https://www.example.com/foo/?bar=baz&inga=42&quux', True),
+    ('www.outremers360.comtalent-de-la-semaine-la-designer-comorienne-aisha-wadaane-je-suis-fiere-de-mes-origines/', False),
     ('lemonde.fr/index.html', True),
     ('foo.com/blah_(wikipedia)#cite-1', True),
     ('223.255.255.254', True),
@@ -36,7 +38,7 @@ NO_PROTOCOL_TESTS = [
     (u'مثال.إختبار', True),
     ('a.b-.co', False),
     ('.www.foo.bar/', False),
-    ('www.foo.bar./', False),
+    ('www.foo.bar./', True),
     ('lemonde.fr/economie/article.php', True),
     ('', False),
     ('    ', False)
@@ -44,6 +46,7 @@ NO_PROTOCOL_TESTS = [
 
 TLD_AWARE_TESTS = [
     ('http://lemonde.fr', True),
+    ('http://www.outremers360.comtalent-de-la-semaine-la-designer-comorienne-aisha-wadaane-je-suis-fiere-de-mes-origines/', False),
     ('https://lemonde.co.uk', True),
     ('http://lemonde.mesfesses', False),
     ('lemonde.watashiwa', False),
--- ural/patterns.py
+++ ural/patterns.py
@@ -3,13 +3,56 @@ import re
 
 PROTOCOL_RE = re.compile(r'^[a-zA-Z0-9]*:?//')
 
-URL_RE_BASE = r"(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:/[^\s]*)?"
+# Taken from:
+#  https://gist.github.com/dperini/729294
+#  https://gist.github.com/pchc2005/b5f13e136a9c9bb2984e5b92802fc7c9
+URL_RE_BASE = URL_REGEX = (
+    # protocol identifier
+    # "(?:(?:(?:https?|ftp):)?//)"
+    # user:pass authentication
+    r"(?:\S+(?::\S*)?@)?"
+    r"(?:"
+    # IP address exclusion
+    # private & local networks
+    r"(?!(?:10|127)(?:\.\d{1,3}){3})"
+    r"(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})"
+    r"(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})"
+    # IP address dotted notation octets
+    # excludes loopback network 0.0.0.0
+    # excludes reserved space >= 224.0.0.0
+    # excludes network & broadcast addresses
+    # (first & last IP address of each class)
+    r"(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])"
+    r"(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}"
+    r"(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"
+    r"|"
+    # host & domain names, may end with dot
+    # can be replaced by a shortest alternative
+    # r"(?![-_])(?:[-\w\u00a1-\uffff]{0,63}[^-_]\.)+"
+    # r"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
+    # # domain name
+    # r"(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*"
+    r"(?:"
+    r"(?:"
+    r"[a-z0-9\u00a1-\uffff]"
+    r"[a-z0-9\u00a1-\uffff_-]{0,62}"
+    r")?"
+    r"[a-z0-9\u00a1-\uffff]\."
+    r")+"
+    # TLD identifier name, may end with dot
+    r"(?:[a-z\u00a1-\uffff]{2,}\.?)"
+    r")"
+    # port number (optional)
+    r"(?::\d{2,5})?"
+    # resource path (optional)
+    r"(?:[/?#]\S*)?"
+)
 
 URL_RE = re.compile(
-    r'^([a-zA-Z0-9]*:?//)?%s$' % URL_RE_BASE)
+    r'^([a-zA-Z0-9]*:?//)?%s$' % URL_RE_BASE, re.I | re.UNICODE)
 
 URL_IN_TEXT_RE = re.compile(
-    r'([a-zA-Z0-9]*:?//)%s' % URL_RE_BASE)
+    r'([a-zA-Z0-9]*:?//)%s' % URL_RE_BASE, re.I | re.UNICODE)
 
 HTML_URL_RE = re.compile(
     r"<a\s.*?href=(?:\"([.#]+?)\"|\'([.#]+?)\'|([^\s]+?))(?:>|\s.*?>)(?:.*?)<[/ ]?a>",
