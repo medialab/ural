6c55a69c0b86a3a75e4455b4eaf86c45a8015439
Yomguithereal <guillaumeplique@gmail.com>
2019-10-30 11:01:29 +0100

    Tweaking LRU serialization unit tests

--- test/lru_serialization_test.py
+++ test/lru_serialization_test.py
@@ -12,16 +12,17 @@ SERIALIZE_TESTS = [
     (['h:bar', 'h:foo'], 'h:bar|h:foo|')
 ]
 
-UNSERIALIZE_TESTS = [
-    ('t:8000|h:fr|h:lemonde|p:article|p:1234|q:query=mobile|', ['t:8000', 'h:fr', 'h:lemonde', 'p:article', 'p:1234', 'q:query=mobile']),
-    ('h:com|h:example|p:wpstyle|p:|q:p=364&q=365|', ['h:com', 'h:example', 'p:wpstyle', 'p:', 'q:p=364&q=365']),
-    ('h:bar|h:foo|', ['h:bar', 'h:foo'])
-]
 
+class TestLruSerialization(object):
+    def test_serialize(self):
+        for lru, serialized in SERIALIZE_TESTS:
+            assert serialize_lru(lru) == serialized
 
-class TestSerializationLru(object):
-    def test_basics(self):
-        for lru, lruSerialized in SERIALIZE_TESTS:
-            assert serialize_lru(lru) == lruSerialized
-        for lruSerialized, lru in UNSERIALIZE_TESTS:
-            assert unserialize_lru(lruSerialized) == lru
+    def test_unserialized(self):
+        for lru, serialized in SERIALIZE_TESTS:
+            assert unserialize_lru(serialized) == lru
+
+    def test_idempotency(self):
+        for lru, serialized in SERIALIZE_TESTS:
+            assert unserialize_lru(serialize_lru(lru)) == lru
+            assert serialize_lru(unserialize_lru(serialized)) == serialized
