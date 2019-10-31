5d37d528205e988c979981775d437da7419ae40f
Yomguithereal <guillaumeplique@gmail.com>
2019-10-31 17:23:11 +0100

    Scaffolding Youtube arch

---
+++ ural/youtube.py
@@ -0,0 +1,12 @@
+# =============================================================================
+# Ural Youtube-related heuristic functions
+# =============================================================================
+#
+# Collection of functions related to Youtube urls.
+#
+
+YOUTUBE_DOMAINS = [
+    'youtube.*',
+    'youtu.be',
+    'youtube.googleapis.*'
+]
