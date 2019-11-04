521028f41bf8b0f8aff72eb70c6d04978055ca21
Yomguithereal <guillaumeplique@gmail.com>
2019-11-04 15:49:20 +0100

    Analysis script

---
+++ scripts/analysis.py
@@ -0,0 +1,38 @@
+import csv
+from urllib.parse import urlsplit, parse_qsl
+from collections import Counter
+from tqdm import tqdm
+
+TOP = 50
+
+FRAGMENTS = Counter()
+QUERIES = Counter()
+QUERIES_COMBO = Counter()
+
+with open('./scripts/data/urls.csv') as f:
+    for line in tqdm(f, desc='Reading urls'):
+        url = line.strip()[1:-1]
+        parsed = urlsplit(url)
+
+        FRAGMENTS[parsed.fragment] += 1
+
+        if parsed.query:
+            for name, value in parse_qsl(parsed.query):
+                QUERIES[name] += 1
+                QUERIES_COMBO['%s=%s' % (name, value)] += 1
+
+def report(name, counter):
+    print()
+
+    title = 'Top %i %s:' % (TOP, name)
+    print(title)
+    print('-' * len(title))
+
+    for record in counter.most_common(TOP):
+        print('  • %s - %i' % record)
+
+    print()
+
+report('fragments', FRAGMENTS)
+report('queries', QUERIES)
+report('queries combo', QUERIES_COMBO)
