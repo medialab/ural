b67989683aa168deb056f2a9728b9597a3faba1a
Yomguithereal <guillaumeplique@gmail.com>
2019-11-07 15:44:10 +0100

    Fixing more mistakes

--- test/youtube_test.py
+++ test/youtube_test.py
@@ -25,11 +25,11 @@ IS_VIDEO_TESTS = [
 PARSE_TESTS = [
     (
         'https://www.youtube.com/watch?v=92HWiOdpY2s',
-        YoutubeVideo(id='92HWiOdpY2s', user=None)
+        YoutubeVideo(id='92HWiOdpY2s')
     ),
     (
         'https://www.youtube.com/watch?id=4526&v=92HWiOdpY2s',
-        YoutubeVideo(id='92HWiOdpY2s', user=None)
+        YoutubeVideo(id='92HWiOdpY2s')
     ),
     (
         'http://www.youtube.com/user/ojimfrance',
@@ -45,11 +45,11 @@ PARSE_TESTS = [
     ),
     (
         'http://www.youtube.com/v/j1i_l0OeeMc',
-        YoutubeVideo(id='j1i_l0OeeMc', user=None)
+        YoutubeVideo(id='j1i_l0OeeMc')
     ),
     (
         'http://www.youtube.com/video/8CCuerpdwnw',
-        YoutubeVideo(id='8CCuerpdwnw', user=None)
+        YoutubeVideo(id='8CCuerpdwnw')
     ),
     (
         'http://www.youtube.com/c/NadineMoranoOfficiel',
@@ -57,7 +57,7 @@ PARSE_TESTS = [
     ),
     (
         'http://www.youtube.com/embed/sC4l-LvAH_c?autoplay=1',
-        YoutubeVideo(id='sC4l-LvAH_c', user=None)
+        YoutubeVideo(id='sC4l-LvAH_c')
     ),
     (
         'http://www.youtube.com/channel/UCWvUxN9LAjJ-sTc5JJ3gEyA',
@@ -69,11 +69,11 @@ PARSE_TESTS = [
     ),
     (
         'https://www.youtube.com/signin?app=desktop&action_handle_signin=true&hl=fr&next=%2Fwatch%3Fv%3DyEtmZKE5jhw&pli=1&auth=awfzXEmX9BuMCCEWwI2MBuBdDBNxbe9_9ApWQnC_LyUJrV06XI3J4TSMOYXwSrdMjoOw',
-        YoutubeVideo(id='yEtmZKE5jhw', user=None)
+        YoutubeVideo(id='yEtmZKE5jhw')
     ),
     (
         'https://accounts.youtube.com/accounts/SetSID?ssdc=1&sidt=ALWU2ctpK5UgBHaSlYgNAS6kKr5K2ViktmHTEQ46AaVcyfB9Ae5jfhUmnJ5XuJTojZDXSFe3pvpYgPf2bpZ58EWKSst0QSJN20EyKnRRoHx9TF4k3xJ14%2F1VNso2ULDtZP1UwAtvqQ2gIYFo%2Bb6SGlmlnZBeBx1rJEDlJKmXbE5j1NiZfQp%2BLYGunuhjot9yZXOrzB2jq6O2CXEkEOw%2Bv23hWOXuUsUaXam2bZ4iRtEFt0EPkMK5rNysv%2FsVs%2F0LJtXTXIOaFGkaTJMbn28zC4VOO2dODGdm8UdGDrDHaI%2FHsiOrasfhK9O182PSjpLmYcLQObLVm9Ai&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Fapp%3Ddesktop%26action_handle_signin%3Dtrue%26hl%3Dfr%26next%3D%252Fwatch%253Fv%253DyEtmZKE5jhw%26pli%3D1%26auth%3DawfzdXEmX9BuMCCEWwI2MBuBdDBNxbe9_9ApWQnC_LyUJrVN06XI3J4TSMOYXwSrdMjoOw',
-        YoutubeVideo(id='yEtmZKE5jhw', user=None)
+        YoutubeVideo(id='yEtmZKE5jhw')
     ),
     (
         'https://www.youtube.com/PressecitronHD/',
@@ -85,15 +85,15 @@ PARSE_TESTS = [
     ),
     (
         'http://youtu.be/afa-5HQHiAs',
-        YoutubeVideo(id='afa-5HQHiAs', user=None)
+        YoutubeVideo(id='afa-5HQHiAs')
     ),
     (
         'http://youtu.be/afa-5HQHiAs/',
-        YoutubeVideo(id='afa-5HQHiAs', user=None)
+        YoutubeVideo(id='afa-5HQHiAs')
     ),
     (
         'http://youtu.be/4SpnqbXd8A8/video',
-        YoutubeVideo(id='4SpnqbXd8A8', user=None)
+        YoutubeVideo(id='4SpnqbXd8A8')
     ),
     (
         'http://youtu.be/aHQHiAs',
@@ -101,15 +101,27 @@ PARSE_TESTS = [
     ),
     (
         'https://youtu.be/rbXhnI_E0hQ%5D',
-        YoutubeVideo(id='rbXhnI_E0hQ', user=None)
+        YoutubeVideo(id='rbXhnI_E0hQ')
     ),
     (
         'http://youtu.be/4SpnqbXd8A8%5B/video',
-        YoutubeVideo(id='4SpnqbXd8A8', user=None)
+        YoutubeVideo(id='4SpnqbXd8A8')
     ),
     (
         'http://www.youtube.com/watch?v=3JqLhV80Vyg%20%20%28%20http%3A%2F%2Fyoutu.be%2F3JqLhV80Vyg%20%29',
-        YoutubeVideo(id='3JqLhV80Vyg', user=None)
+        YoutubeVideo(id='3JqLhV80Vyg')
+    ),
+    (
+        'https://m.youtube.com/?hl=fr&gl=FR#%2Fwatch%3Fv%3DObJTChxhhvY',
+        YoutubeVideo(id='ObJTChxhhvY')
+    ),
+    (
+        'https://www.youtube.com/?hl=fr&gl=FR&app=desktop#/watch?v=ObJTChxhhvY',
+        YoutubeVideo(id='ObJTChxhhvY')
+    ),
+    (
+        'http://www.youtube.com/v/mZiqexz7aqQ%26hl%3Den%26fs%3D1',
+        YoutubeVideo(id='mZiqexz7aqQ')
     )
 ]
 
--- ural/youtube.py
+++ ural/youtube.py
@@ -19,8 +19,9 @@ YOUTUBE_VIDEO_ID_RE = re.compile(r'^[a-zA-Z0-9_-]{11}$')
 QUERY_V_RE = re.compile(QUERY_VALUE % r'v', re.I)
 NEXT_V_RE = re.compile(r'next=%2Fwatch%3Fv%3D([^%&]+)', re.I)
 NESTED_NEXT_V_RE = re.compile(r'next%3D%252Fwatch%253Fv%253D([^%&]+)', re.I)
+FRAGMENT_V_RE = re.compile(r'^(?:%2F|/)watch(?:%3F|\?)v(?:%3D|=)([a-zA-Z0-9_-]{11})', re.I)
 
-YoutubeVideo = namedtuple('YoutubeVideo', ['id', 'user'])
+YoutubeVideo = namedtuple('YoutubeVideo', ['id'])
 YoutubeUser = namedtuple('YoutubeUser', ['id', 'name'])
 YoutubeChannel = namedtuple('YoutubeChannel', ['id', 'name'])
 
@@ -51,12 +52,23 @@ def is_youtube_video_id(value):
 
 
 def parse_youtube_url(url, fix_common_mistakes=True):
+    """
+    Function parsing the given url and returning either a YoutubeUser,
+    YoutubeChannel, YoutubeVideo or None if nothing of information could be
+    found.
+
+    Args:
+        url (str): Url to parse.
+        fix_common_mistakes (bool, optional): Whether to fix common mistakes
+            in Youtube urls as you can find them on the web. Defaults to `True`.
+
+    """
 
     # Continuation urls
     m = NEXT_V_RE.search(url) or NESTED_NEXT_V_RE.search(url)
 
     if m:
-        return YoutubeVideo(id=m.group(1), user=None)
+        return YoutubeVideo(id=m.group(1))
 
     # Parsing
     if isinstance(url, SplitResult):
@@ -68,7 +80,7 @@ def parse_youtube_url(url, fix_common_mistakes=True):
     if not is_youtube_url(parsed):
         return
 
-    _, _, path, query, _ = parsed
+    _, _, path, query, fragment = parsed
 
     # youtu.be
     if parsed.hostname.endswith('youtu.be'):
@@ -82,9 +94,21 @@ def parse_youtube_url(url, fix_common_mistakes=True):
             if not is_youtube_video_id(v):
                 return
 
-            return YoutubeVideo(id=v, user=None)
+            return YoutubeVideo(id=v)
 
         return
+
+    # Hidden video in fragment
+    if fragment:
+        mv = FRAGMENT_V_RE.match(fragment)
+
+        if mv:
+            v = mv.group(1)
+
+            if not is_youtube_video_id(v):
+                return
+
+            return YoutubeVideo(id=v)
 
     # Typical video url
     if path == '/watch':
@@ -99,7 +123,7 @@ def parse_youtube_url(url, fix_common_mistakes=True):
             if not is_youtube_video_id(v):
                 return
 
-            return YoutubeVideo(id=v, user=None)
+            return YoutubeVideo(id=v)
 
     # Video file
     elif (
@@ -109,10 +133,13 @@ def parse_youtube_url(url, fix_common_mistakes=True):
     ):
         v = path.rsplit('/', 1)[-1]
 
+        if fix_common_mistakes:
+            v = v[:11]
+
         if not is_youtube_video_id(v):
             return
 
-        return YoutubeVideo(id=v, user=None)
+        return YoutubeVideo(id=v)
 
     # Typical user url
     elif path.startswith('/user/'):
