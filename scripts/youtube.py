e372c736fdf1332bef19d99e263bf659f52e6c49
Yomguithereal <guillaumeplique@gmail.com>
2019-11-07 13:09:06 +0100

    Sanity checking youtube urls

--- scripts/youtube.py
+++ scripts/youtube.py
@@ -1,9 +1,12 @@
 import csv
+from ural.youtube import is_youtube_url
 
 with open('./scripts/data/youtube-urls.csv') as f:
     reader = csv.reader(f)
+    next(reader)
 
     for line in reader:
         youtube_url = line[1]
 
-        print(youtube_url)
+        # if not is_youtube_url(youtube_url):
+        #     print(youtube_url)
