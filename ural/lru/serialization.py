6a4a434ea8b897571749269ba3d2d587699171aa
Yomguithereal <guillaumeplique@gmail.com>
2019-11-15 11:55:53 +0100

    More robust unserialize_lru

--- ural/lru/serialization.py
+++ ural/lru/serialization.py
@@ -4,12 +4,15 @@
 #
 # Functions related to LRU serialization.
 #
+import re
+
+SERIALIZED_LRU_SPLITTER_RE = re.compile(r'\|(?=[shtpqfuw]:)')
 
 
 def serialize_lru(stems):
     return '|'.join(stems) + '|'
 
 
-def unserialize_lru(serial):
-    serial = serial.rstrip('|')
-    return serial.split('|')
+def unserialize_lru(lru):
+    lru = lru.rstrip('|')
+    return SERIALIZED_LRU_SPLITTER_RE.split(lru)
