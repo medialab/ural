8d4ce4d486f81bbc7c95268a33031f33f94332c6
Yomguithereal <guillaumeplique@gmail.com>
2019-12-16 11:42:14 +0100

    safe_urlsplit util

--- ural/utils.py
+++ ural/utils.py
@@ -4,6 +4,9 @@
 #
 # Miscellaneous utilities used throughout the library's code.
 #
+import re
+
+from ural.patterns import PROTOCOL_RE
 
 # PY2/PY3 compatible string_type...
 string_type = str
@@ -34,3 +37,12 @@ except ImportError:
         urlunsplit,
         SplitResult
     )
+
+
+def safe_urlsplit(url, scheme='http'):
+    if not re.match(PROTOCOL_RE, url):
+        url = scheme + '://' + url
+
+    splitted = urlsplit(url)
+
+    return splitted
