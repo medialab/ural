c992e133614b5d65b050e70cf6d5b607f64a780a
d3scmps <jean.descmps@gmail.com>
2020-09-03 16:15:45 +0200

    defining extract_screen_name_from_twitter_url function in the ural.twitter.py script

--- ural/twitter.py
+++ ural/twitter.py
@@ -7,10 +7,11 @@
 import re
 
 from ural.patterns import DOMAIN_TEMPLATE
-from ural.utils import SplitResult
+from ural.utils import SplitResult, urlsplit, urlpathsplit
 
 TWITTER_DOMAINS_RE = re.compile(r'twitter\.com', re.I)
 TWITTER_URL_RE = re.compile(DOMAIN_TEMPLATE % r'(?:[^.]+\.)*twitter\.com', re.I)
+USERNAME_EXCEPTIONS = ['home', 'hashtag', 'search', 'explore', 'settings', 'messages', 'notifications', 'explore', 'i']
 
 
 def is_twitter_url(url):
@@ -30,5 +31,46 @@ def is_twitter_url(url):
     return bool(re.match(TWITTER_URL_RE, url))
 
 
+def normalisation(username):
+    """
+    Small function used in extract_screen_name_from_twitter_url(url) in order to normalize username and deal
+    with specific exceptions.
+    Args:
+        username (str) : username to test.
+
+    Returns:
+        username (str) or rise an exception.
+
+    """
+    if username in USERNAME_EXCEPTIONS:
+        return None
+    else:
+        if username[0] == '@':
+            username = username[1:]
+        username = username.lower()
+    return username
+
+
 def extract_screen_name_from_twitter_url(url):
-    pass
+    """
+    Function returning the screen_name
+
+    Args:
+        url (str) : Url from which we extract the screen_name if it exists
+
+    Returns:
+        str : screen_name if the url is a valid twitter url
+              None otherwise
+
+    """
+    # Checking whether the url is a valid twitter url
+    valid_url = is_twitter_url(url)
+    url_fragmente = urlsplit(url)
+    if valid_url and len(url_fragmente.path) >= 1:
+        if len(url_fragmente.path) != 1:
+            username = url_fragmente.path[1:].split('/')[0]
+        else:
+            username = url_fragmente.fragment[2:]
+        return normalisation(username)
+    else:
+        return None
