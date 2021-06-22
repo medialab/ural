2c4ad19ee29d03789f683ee96c2356f42d6f42be
Yomguithereal <guillaumeplique@gmail.com>
2021-06-22 11:45:41 +0200

    Bump 0.30.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('./README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='ural',
-      version='0.30.3',
+      version='0.30.4',
       description='A helper library full of URL-related heuristics.',
       long_description=long_description,
       long_description_content_type='text/markdown',
