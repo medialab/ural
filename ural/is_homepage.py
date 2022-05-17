830906c79996403467c36ce290962a2a1ab5b892
elanhermi <hlne.herman@gmail.com>
2022-05-17 18:26:09 +0200

    Making beautiful code even more beautiful

--- ural/is_homepage.py
+++ ural/is_homepage.py
@@ -1,9 +1,9 @@
 from ural.utils import safe_urlsplit
 
+
 def is_homepage(url):
     url_tuple = safe_urlsplit(url)
     if url_tuple.path == '':
         return True
     else:
         return False
-
