3c4e5eca0809f210823a930fecf0e9ac10680e77
elanhermi <hlne.herman@gmail.com>
2022-05-23 16:58:25 +0200

    Fixing homepage detector

--- ural/is_homepage.py
+++ ural/is_homepage.py
@@ -1,10 +1,12 @@
+from operator import is_
+from regex import D
 from ural.utils import safe_urlsplit
 import re
 
 
 def is_homepage(url):
     url_tuple = safe_urlsplit(url)
-    if (url_tuple.path == '' or url_tuple.path == '/' or 'index' in url_tuple.path or 'home' in url_tuple.path or 'resultats-elections' in url_tuple.path) and (url_tuple.query == '' or url_tuple.fragment == ''):
+    if (url_tuple.path == '' or url_tuple.path == '/' or 'index' in url_tuple.path or 'home' in url_tuple.path) and (url_tuple.query == '' or url_tuple.fragment == ''):
         return True
     else:
         return False
