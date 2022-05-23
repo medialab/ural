2557c6fc7a436d81393e70f919a7332286541e8e
elanhermi <hlne.herman@gmail.com>
2022-05-23 16:43:58 +0200

    Fixing homepage detector

--- test/is_homepage_test.py
+++ test/is_homepage_test.py
@@ -1,10 +1,21 @@
 from ural import is_homepage
 
 DEFAULT_TESTS = [
-    ('https://www.example.com/foo/?bar=baz&inga=42&quux', False),
-    ('http://www.outremers360.comtalent-de-la-semaine-la-designer-comorienne-aisha-wadaane-je-suis-fiere-de-mes-origines/', False),
-    ('http://lemonde.fr///a/./b/..', False),
-    ('http://www.lemonde.fr', True)
+    ("https://www.lemonde.fr/", True),
+    ("https://www.lemonde.fr", True),
+    ("https://www.lemonde.fr?hello=again", True),
+    ("https://www.lemonde.fr#menu", True),
+    ("lemonde.fr", True),
+    ("https://www.lemonde.fr/resultats-elections/", False),
+    ("http://business.lefigaro.fr/", True),
+    ("https://www.lemonde.fr/index", True),
+    ("https://www.lemonde.fr/index.html", True),
+    ("https://www.lemonde.fr/index.html/", True),
+    ("https://www.lemonde.fr/index.html/?hello", True),
+    ("https://www.lemonde.fr/index.aspx", True),
+    ("https://www.lemonde.fr/home", True),
+    ("https://www.lemonde.fr/home.html", True),
+    ("https://www.modulargrid.net/e/shakmat-modular-dual-dagger-", False)
 ]
 
 
--- ural/is_homepage.py
+++ ural/is_homepage.py
@@ -1,9 +1,10 @@
 from ural.utils import safe_urlsplit
+import re
 
 
 def is_homepage(url):
-    url_tuple = safe_urlsplit(url)
-    if url_tuple.path == '':
-        return True
-    else:
-        return False
+   url_tuple = safe_urlsplit(url)
+   if (url_tuple.path == '' or url_tuple.path == '/' or 'index' in url_tuple.path or 'home' in url_tuple.path or 'resultats-elections' in url_tuple.path) and (url_tuple.query == '' or url_tuple.fragment == ''):
+       return True
+   else:
+       return False
