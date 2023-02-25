119ffa17dc25e427f6236dbe47c9aab303290d43
Yomguithereal <guillaumeplique@gmail.com>
2023-02-25 20:48:39 +0100

    Adding some references

--- ural/classes/tld_trie.py
+++ ural/classes/tld_trie.py
@@ -1,3 +1,7 @@
+from ural.utils import safe_urlsplit
+from ural.has_special_host import is_special_host
+
+
 class TLDTrieNode(object):
     __slots__ = ("children", "exception", "leaf", "private")
 
@@ -10,10 +14,10 @@ class TLDTrieNode(object):
 
 class TLDTrie(object):
     def __init__(self):
-        self.root = TLDTrieNode()
+        self.__root = TLDTrieNode()
 
     def add(self, tld, private=False):
-        node = self.root
+        node = self.__root
 
         # Iterating over the tld parts in reverse order
         for part in reversed(tld.split(".")):
@@ -40,3 +44,59 @@ class TLDTrie(object):
 
         if private:
             node.private = True
+
+    def __match(self, url):
+        parsed = safe_urlsplit(url)
+
+        if is_special_host(parsed.hostname):
+            return None
+
+        hostname = parsed.hostname.lower().rstrip(".")
+        parts = hostname.split(".")
+
+        current_length = 0
+        tld_length = 0
+        match = None
+        l = len(parts)
+
+        node = self.__root
+
+        for i in range(l - 1, -1, -1):
+            part = parts[i]
+
+            # Cannot go deeper
+            if node.children is None:
+                break
+
+            # Exception
+            if part == node.exception:
+                break
+
+            child = node.children.get(part)
+
+            # Wildcards
+            if child is None:
+                child = node.children.get("*")
+
+            # If the current part is not in current node's children, we can stop
+            if child is None:
+                break
+
+            # Else we move deeper and increment our tld offset
+            current_length += 1
+            node = child
+
+            if node.leaf:
+                tld_length = current_length
+                match = node
+
+        # Checking the node we finished on is a leaf and is one we allow
+        if match is None or not match.leaf:
+            return None
+
+        if l == tld_length:
+            non_zero_i = -1  # hostname = tld
+        else:
+            non_zero_i = max(1, l - tld_length)
+
+        return parts, non_zero_i, parsed
--- ural/tld.py
+++ ural/tld.py
@@ -1,3 +1,33 @@
+# =============================================================================
+# Ural TLD-related Functions
+# =============================================================================
+#
+# Some useful links:
+#   - https://wiki.mozilla.org/Public_Suffix_List
+#   - https://publicsuffix.org/list/public_suffix_list.dat
+#   - https://www.iana.org/domains/root/db
+#   - https://data.iana.org/TLD/tlds-alpha-by-domain.txt
+#   - http://en.wikipedia.org/wiki/List_of_Internet_top-level_domains
+#
+# NOTE: there is a debate about what is or is not a TLD per se. This is a very
+# interesting topic and one should note that the current module follows an
+# "operational" definition of a what a TLD is, but not the purist idea of what
+# it actually is actually supposed to be (according to IANA at least). Our
+# definition is in line with Mozilla's one wrt domain attribution and the
+# problem we are trying to address is usually to obtain the accurate domain name
+# from a given url, which is not trivial and means you MUST rely on an exhaustive
+# and maintained list of current possibilities (as represented by Mozilla's
+# public suffix list).
+#
+# For instance, let's observe the following url:
+#   https://www.whatever.co.uk/section/article.html
+#
+# What we need to extract from this url is what is colloquially known as a
+# domain name: "whatever.co.uk"
+#
+# So we will consider this url TLD as being: ".co.uk", while a purist would
+# argue that the actual TLD is ".uk".
+#
 import codecs
 from os.path import dirname, join
 
@@ -6,7 +36,12 @@ try:
 except ImportError:
     from urllib2 import urlopen
 
+import ural.tld_list as tld_list
+from ural.classes.tld_trie import TLDTrie
+
 MOZILLA_PUBLIC_SUFFIX_LIST = "https://publicsuffix.org/list/public_suffix_list.dat"
+
+TLD_TRIE = TLDTrie()
 
 
 def download_suffix_list(url):
@@ -56,3 +91,18 @@ def upgrade_suffix_list(url=MOZILLA_PUBLIC_SUFFIX_LIST):
             f.write('  (%s, "%s"),\n' % (1 if private else 0, tld))
 
         f.write("]\n")
+
+
+def hydrate_tld_trie():
+    global TLD_TRIE
+
+    TLD_TRIE = TLDTrie()
+
+    for private, tld in tld_list.SUFFIXES:
+        TLD_TRIE.add(tld, private=private)
+
+
+hydrate_tld_trie()
+
+# TODO: is_tld, get_tld, get_domain_name
+# TODO: exception, refresh_tld_trie, hostname tokenization
