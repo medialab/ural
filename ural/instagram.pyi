3b7940cb5d0c2f192b32ed1814bface030d1b284
Laura Miguel <112872367+MiguelLaura@users.noreply.github.com>
2023-03-02 13:53:51 +0100

    Adding Instagram reel url parsing

--- ural/instagram.pyi
+++ ural/instagram.pyi
@@ -4,14 +4,17 @@ from ural.types import AnyUrlTarget
 class InstagramUser(NamedTuple):
     name: str
 
-class InstagramPost(NameError):
+class InstagramPost(NamedTuple):
     id: str
     name: str
+
+class InstagramReel(NamedTuple):
+    id: str
 
 def is_instagram_post_shortcode(value: str) -> bool: ...
 def is_instagram_username(value: str) -> bool: ...
 def is_instagram_url(url: AnyUrlTarget) -> bool: ...
 def parse_instagram_url(
     url: AnyUrlTarget,
-) -> Optional[Union[InstagramUser, InstagramPost]]: ...
+) -> Optional[Union[InstagramUser, InstagramPost, InstagramReel]]: ...
 def extract_username_from_instagram_url(url: AnyUrlTarget) -> Optional[str]: ...
