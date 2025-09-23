2d11508d934c8dd45a2a04c3a3743e4676def6ea
echevale <evan.chevalerias@gmail.com>
2025-09-23 09:25:22 +0200

    Corrected false shortened url within is_shortened

--- test/is_shortened_test.py
+++ test/is_shortened_test.py
@@ -31,7 +31,9 @@ TESTS = [
     ("https://share.google/2cxtlcnlG7nkGn0tV", True),
     ("https://fefd.link/OAk8r", True),
     ("https://ara.tv/k23h6", True),
-    ("https://www.h24.news/S7G", True)
+    ("https://www.h24.news/S7G", True),
+    ("https://l.smartnews.com/p-5M5V72PS/7Kl8bk", False), # For now
+    ("https://open.substack.com/pub/demcastusa/p/britain-shows-trump-the-door-no-crown", False),
 ]
 
 
